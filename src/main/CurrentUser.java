92d1ef29038b03b6d15f3463ab49ba4e80a59464
Najwa <nanaikhwanm69@gmail.com>
2024-11-04 18:22:59 +0700

    first commit

---
+++ src/main/CurrentUser.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+/**
+ *
+ * @author binta
+ */
+public class CurrentUser {
+    public static String username; // Variabel statis untuk menyimpan username saat ini
+}
